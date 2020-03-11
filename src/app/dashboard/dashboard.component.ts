9b49784a4b7d324b0395fbb100e8ac810f7a4457
NwotYknuf <jlafille@ensc.fr>
2020-03-11 15:07:41 +0100

    alzeaze

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Actu } from '../Actu';
+import { User } from '../User';
+import { UtilisateursService } from '../utilisateurs.service'
+import { ActuService } from '../actu.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,11 +11,26 @@ import { Component, OnInit } from '@angular/core';
 
 export class DashboardComponent implements OnInit {
 
-  constructor() {
+  public actu: Actu;
+  public user: User;
+
+  constructor(private actuService: ActuService, private userService: UtilisateursService) {
 
   }
 
   ngOnInit() {
+    let _users;
+    let _actus;
+
+    this.userService.getUsers().subscribe(users => {
+      _users = users;
+      this.user = _users[Math.floor(Math.random() * _users.length)];
+    });
+
+    this.actuService.getActu().subscribe(actus => {
+      _actus = actus;
+      this.actu = _actus[Math.floor(Math.random() * _users.length)];
+    });
 
   }
 
